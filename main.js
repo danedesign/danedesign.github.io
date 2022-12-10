791e4b324112acb1b94d6a08457932136f7551e5
daienlyu <daienlyu@foxmail.com>
2022-12-10 21:20:22 +1100

    Optimised Directory System
    - Imported JQuery for including header, footer and social media window for all pages. All at once.
    - Optimised Personal Naming
    - Replaced all relative directory with indefinite ones

---
+++ main.js
@@ -0,0 +1,6 @@
+// This one is to include the header, footer and social media elements into all pages
+$(function() {
+    $("header").load("/components/header.html");
+    $("#contact").load("/components/social-media.html");
+    $("footer").load("/components/footer.html");
+})
