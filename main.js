b2381134c8ea05be9acd75ea83eb1967fac92c65
dane.design <daienlyu@foxmail.com>
2023-01-10 16:14:57 +1100

    loading screen hidden test

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ $(function () {
     $("#contact").load("/components/social-media.html");
     $("footer").load("/components/footer.html");
 })
+
+// var loader = document.getElementsByClassName("splash-screen");
+
+window.addEventListener("load", function () {
+    $('.splash-screen').hide();
+//   loader.style.display = "none";
+});
+
+// $('window').on("load", function() {
+//     $(this).hide();
+// })
