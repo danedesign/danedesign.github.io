8b1b89029fdfbeb707bb3f1a8a7213410961fca1
dane.design <daienlyu@foxmail.com>
2023-01-10 16:29:32 +1100

    Intersected Observer Test

--- main.js
+++ main.js
@@ -5,13 +5,38 @@ $(function () {
     $("footer").load("/components/footer.html");
 })
 
-// var loader = document.getElementsByClassName("splash-screen");
 
 window.addEventListener("load", function () {
     $('.splash-screen').hide();
-//   loader.style.display = "none";
 });
 
-// $('window').on("load", function() {
-//     $(this).hide();
-// })
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getElementbyId('headline-ywaste');
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 700px 0px"
+};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image);
+})
