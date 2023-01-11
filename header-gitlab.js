bbe114620bcef2298c6dcb62c6a6a3b8cac31c6b
dane.design <daienlyu@foxmail.com>
2023-01-11 21:32:52 +1100

    New Design, running out of rounded felt!

--- header-gitlab.js
+++ header-gitlab.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     $('#section-more-trigger').click(function() {
         $("#section-more").fadeToggle();
         $('#section-more-trigger').hide();
-    })    
+    })
 
     $("a#lang-change").click(function () {
         $('[lang="en"]').toggle();
@@ -38,14 +38,22 @@ $(document).ready(function () {
     )
 
     $("a.awards-show-btn").click(function () {
-        $("#section-awards").css('padding-top', '33%');
-        $("#section-awards").css('padding-bottom', '+50px');
+        $("#section-awards").css('padding-top', '35%');
+        $('.awards-left-portion').css('color', '#fff');
+        $('.awards-pull-text').css('color', 'rgba(255, 255, 255, 0)');
+        $('.awards-right-portion, .awards-left-portion').css('opacity', '0');
+        // $('header').css('background', 'none');
+        // $("#section-awards").css('padding-bottom', '+50px');
         $(".awards-showreel-vid-desktop, .awards-close-btn").fadeIn("slow");
+        $(".awards-showreel-vid-desktop").trigger('play');
     })
 
     $("iconify-icon.awards-close-btn").click(function () {
         $("#section-awards").css('padding-top', '');
-        $("#section-awards").css('padding-bottom', '');
+        $('.awards-left-portion').css('color', '');
+        $('.awards-pull-text').css('color', '');
+        $('.awards-right-portion, .awards-left-portion').css('opacity', '');
+        // $("#section-awards").css('padding-bottom', '');
         $(".awards-showreel-vid-desktop, .awards-close-btn").fadeToggle("slow");
         $(".awards-showreel-vid-desktop").trigger('pause');
     })
