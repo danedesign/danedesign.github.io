6d498b80ce7be2b70b9f3314571299c6adb4d4bf
dane.design <daienlyu@foxmail.com>
2023-01-14 01:03:14 +1100

    Facebook Messenger Chat Plugin Test 2

--- main.js
+++ main.js
@@ -6,26 +6,6 @@ $(function () {
     $('#fixed-messenger').load("/components/messenger.html");
 })
 
-var chatbox = document.getElementById('fb-customer-chat');
-chatbox.setAttribute("page_id", "110222261435384");
-chatbox.setAttribute("attribution", "biz_inbox");
-
-window.fbAsyncInit = function() {
-  FB.init({
-    xfbml            : true,
-    version          : 'v15.0'
-  });
-};
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-
 // window.addEventListener("load", function () {
 //     $('.splash-screen').hide();
 // });
